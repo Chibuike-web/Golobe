64823b987c796cddb79735f0f893f6e06099840b
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-04-14 20:06:17 +0100

    refactor

--- frontend/src/LandingPage/Navbar/Navbar.tsx
+++ frontend/src/LandingPage/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AirplaneIcon, BedIcon } from "../../assets/Icons";
 import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
-import Logo from "../../assets/LandingPage/Logo.svg";
+import ColourLogo from "../../assets/LandingPage/ColourLogo.svg";
 
 export default function Navbar() {
 	const [scrollHeight, setScrollHeight] = useState(false);
@@ -23,7 +23,7 @@ export default function Navbar() {
 
 	return (
 		<nav
-			className={` flex items-center justify-between p-[2rem] w-full z-[100] mx-auto max-w-[88rem] md:px-4 md:py-4 ${
+			className={` flex items-center justify-between px-[6rem] py-[2rem] w-full z-[100] mx-auto max-w-[96rem] md:px-4 md:py-4 ${
 				scrollHeight ? styles.sticky : styles.normal
 			}`}
 			aria-label="Main Navigation"
@@ -31,31 +31,48 @@ export default function Navbar() {
 			<ul className="flex gap-8 md:hidden">
 				<li>
 					<Link to="/flightsearch" className="flex space-x-1 items-center">
-						<AirplaneIcon color="white" />
-						<span className="text-sm font-semibold text-white">Find Flight</span>
+						<AirplaneIcon color={scrollHeight ? "#112211" : "white"} />
+						<span
+							className={`text-sm font-semibold ${
+								scrollHeight ? "text-blackishGreen" : "text-white"
+							}`}
+						>
+							Find Flight
+						</span>
 					</Link>
 				</li>
 				<li>
 					<Link to="/hotelsearch" className="flex space-x-1 items-center">
-						<BedIcon color="white" />
-						<span className="text-sm font-semibold text-white">Find Stays</span>
+						<BedIcon color={scrollHeight ? "#112211" : "white"} />
+						<span
+							className={`text-sm font-semibold ${
+								scrollHeight ? "text-blackishGreen" : "text-white"
+							}`}
+						>
+							Find Stays
+						</span>
 					</Link>
 				</li>
 			</ul>
 			<figure>
 				<img
-					src={scrollHeight ? Logo : GolobeLogo}
+					src={scrollHeight ? ColourLogo : GolobeLogo}
 					alt="Golobe Travel Logo"
 					className="w-full max-w-24"
 				/>
 			</figure>
 			<div className="flex gap-[1.875rem] items-center md:hidden">
-				<Link to="/login" className="text-white text-sm font-semibold">
+				<Link
+					to="/login"
+					className={`${scrollHeight ? "text-blackishGreen" : "text-white"} text-sm font-semibold`}
+				>
 					Login
 				</Link>
 				<Link
 					to="/signup"
-					className="text-sm font-semibold bg-white text-blackishGreen px-[1.5rem] py-[0.75rem] rounded-[0.5rem]"
+					className={`text-sm font-semibold ${
+						scrollHeight ? "bg-mintGreen" : "bg-white"
+					} text-blackishGreen px-[1.5rem] py-[0.75rem] rounded-[0.5rem]`}
 				>
 					Sign up
 				</Link>
