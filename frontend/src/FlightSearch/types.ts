fd93ceda087fc62ba80d10ac56ea723f07254ed2
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-05-18 00:17:09 +0100

    Updates

--- frontend/src/FlightSearch/types.ts
+++
@@ -1,7 +0,0 @@
-export type Detail = {
-	id: string;
-	city: string;
-	subCopy: string;
-	price: number;
-	image: string;
-};
--- frontend/src/Navbar/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import NavbarSignedIn from "./NavbarSignedIn";
-import NavbarGuest from "./NavbarGuest";
-
-export default function Navbar() {
-	const [isSignedIn, setIsSignedIn] = useState(false);
-	return <>{isSignedIn ? <NavbarSignedIn /> : <NavbarGuest />}</>;
-}
